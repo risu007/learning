85d55aa9335e85a726523a208559545780ec3f25
be1048716 <hemantchowdhury1998@gmail.com>
2018-10-25 18:45:33 +0530

    complete

--- coin change.cpp
+++ coin change.cpp
@@ -1,17 +1,27 @@
+#include<bits/stdc++.h>
 using namespace std;
 
-int f(int n,int *a,int ini,int size)
+int dp[1000][1000];
+int a[1000];
+int size;
+
+int f(int n,int ini)
 {
-	int i,sum=0,temp;
-	for(i=ini;i<size;i++)
+	if(n==0)
+	return 1;
+	if(n<0)
+	return 0;
+	if(ini==size)
+	return 0;
+	if(dp[n][ini]==-1)
 	{
-		temp=n-a[i];
-		if(temp==0)
-			sum++;
-		else if(temp>0)
-			sum+=f(temp,a,i,size);
+		dp[n][ini]=0;
+		for(int i=ini;i<size;i++)
+		{
+			dp[n][ini]+=f(n-a[i],i);
+		}
 	}
-	return sum;
+	return dp[n][ini];
 }
 int main()
 {
@@ -19,13 +29,15 @@ int main()
 	cin>>t;
 	while(t--)
 	{
-	int n,size;
+		
+	int n;
+	memset(dp,-1,sizeof dp);
 	cin>>size;
-	int a[size];
 	for(int i=0;i<size;i++)
 		cin>>a[i];
 	cin>>n;
-	cout<<f(n,a,0,size)<<endl;
+	
+	cout<<f(n,0)<<endl;
 	}
 	return 0;
 }

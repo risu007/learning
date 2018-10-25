9ac7a95a86c363967f276f0dff33e1faf3d3d885
be1048716 <hemantchowdhury1998@gmail.com>
2018-10-25 19:24:27 +0530

    farida added

--- farida.cpp
+++ farida.cpp
@@ -1,28 +1,28 @@
 #include<bits/stdc++.h>
-#define ll long long int
-#define pii pair<ll,ll>
-#define mp make_pair
-#define pb push_back
-#define all(v) v.begin(),v.end()
-#define ff first
-#define ss second
-#define M 500005
-#define mod 1000000007
-#define inf LLONG_MAX
 using namespace std;
-
+#define ll long long int
+ll f(ll *a,ll n,ll ini)
+{
+	ll sum,temp=0,i;
+	for(i=ini;i<n;i++)
+	{
+		sum=a[i]+f(a,n,i+2);
+		temp=max(sum,temp);
+	}
+	return temp;
+}
 int main()
 {
-	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	ll t,n,m,q,i,j,k,x,y,z;
-
+	int t,j=1;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		ll a[n],i;
+		for(i=0;i<n;i++)
+			cin>>a[i];
+		cout<<"Case "<<j++<<": "<<f(a,n,0)<<endl;
+	}
+	return 0;
 }
-
-// Name - Hemant Chowdhury
-// Arise, Awake And Stop Not Till The Goal Is Reached!
-// If I fail,I will stand again;If I win,I will move farther
-
-
-
-
-
